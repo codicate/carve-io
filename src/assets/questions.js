1d38ad40f3e1fc8afaa310607e7e4976c9f42798
codicate <codicate@gmail.com>
2022-06-12 16:08:33 -0400

    questions

--- src/assets/questions.js
+++ src/assets/questions.js
@@ -1,128 +1,243 @@
 const questions = [
 	{
-		question: '',
-		choices: ['', '', '', ''],
+		question: 'Which of the following is a microeconomic concept?',
+		choices: [
+			'stock exchange',
+			'personal finance',
+			'market failure',
+			'trust fund',
+		],
+		correctAnswer: 2,
+	},
+	{
+		question:
+			'The concept of diminishing return explains which of the following phenomena?',
+		choices: [
+			'A worker cannot maintain the same level of output after long hours of work',
+			'When the price of a good rises, people will buy less of it',
+			'Company will benefit more from buying captial goods than hiring additional workers',
+			'A worker will be less producitive if he is paid less',
+		],
 		correctAnswer: 0,
 	},
 	{
-		question: '',
-		choices: ['', '', '', ''],
+		question:
+			'A farmer invested in his wheat farm and made a profit of $100. Which one of the following is the opportunity cost?',
+		choices: [
+			'Soil needs time to heal from the farming',
+			'Wheat lost due to pests',
+			'Money spent on fertilizer',
+			'Profit from planting a different crop',
+		],
+		correctAnswer: 3,
+	},
+	{
+		question:
+			'Should a wealthy country ever trade with a poor country that is less productive in every industry?',
+		choices: [
+			'No, because of the concept of absolute advantage',
+			'Yes, because of the concept of comparative advantage',
+			'Yes, because of the concept of absolute advantage',
+			'Yes, because of the concept of diminishing return',
+		],
 		correctAnswer: 0,
 	},
 	{
-		question: '',
-		choices: ['', '', '', ''],
+		question: 'Which of the following is not shifter of demand curve?',
+		choices: ['population', 'income', 'future expectation', 'technology'],
+		correctAnswer: 3,
+	},
+	{
+		question:
+			'Which of the following is an example of a non-excludable rivalry good?',
+		choices: ['public good', 'club good', 'natural resources', 'private good'],
+		correctAnswer: 2,
+	},
+	{
+		question:
+			'Which of the following concept explains why transfer trip so much cheaper than direct trip?',
+		choices: [
+			'diminishing returns',
+			'price discrimmination',
+			'concept of time as a resource',
+			'ecnonomics of scale',
+		],
+		correctAnswer: 1,
+	},
+	{
+		question:
+			'When the price of peanut increases by 20 percent, the quantity demanded of jelly decreases by 40 percent. What is the cross-price elasticity of the two goods?',
+		choices: ['0.2', '-0.2', '-2', '2'],
+		correctAnswer: 2,
+	},
+	{
+		question:
+			'Which of the following is the least likely way to increase the amount of healthcare that people provide their children?',
+		choices: [
+			'reduce the distance traveled to get healthcare',
+			'subsidize healthcare',
+			'provide coupons for free healthcare',
+			'increase the price of healthcare',
+		],
 		correctAnswer: 0,
 	},
 	{
-		question: '',
-		choices: ['', '', '', ''],
+		question:
+			'Which one of the following is not a reason why we study microecnonomics?',
+		choices: [
+			'determine how goods can be best allocated for various goods and services',
+			'determine the best course of action for individual firms',
+			'understand the principle of demand and supply to be able to keep the company afloat',
+			'understand the principle of economics on a individual level to be able to make better financial decisions',
+		],
+		correctAnswer: 3,
+	},
+	{
+		question: 'Which of the following is an example of negative externality?',
+		choices: [
+			'Drinking too much coffee can make a person feel ill',
+			'Too many new firms opening causes existing firms to lose profit',
+			'Opening of a new factory causes the neighborhood real estate value to drop',
+			'A new technology allows some firms to make more profit but not all firms',
+		],
+		correctAnswer: 2,
+	},
+	{
+		question: 'What Microeconomics is about?',
+		choices: [
+			'Study of Business Environment',
+			'Study of the Economy at Micro Level',
+			'Study of financial position of the economy',
+			'None of the above',
+		],
+		correctAnswer: 1,
+	},
+	{
+		question:
+			'Which one of the following is not an example of non-price competition?',
+		choices: [
+			'Charge less for specific services',
+			'Advertising',
+			'Membership card',
+			'Loyalty Program',
+		],
 		correctAnswer: 0,
 	},
 	{
-		question: '',
-		choices: ['', '', '', ''],
+		question: 'While in Perfect Competition',
+		choices: [
+			'Input prices are given',
+			'Buyers are independent',
+			'Firms are price taker',
+			'None of the above',
+		],
 		correctAnswer: 0,
 	},
 	{
-		question: '',
-		choices: ['', '', '', ''],
+		question:
+			'Oligopolies can end up looking like competitive markets if the number of firms is',
+		choices: [
+			'small and they all cooperate',
+			'small and they do not cooperate',
+			'large and they all cooperate',
+			'large and they do not cooperate',
+		],
+		correctAnswer: 3,
+	},
+	{
+		question: 'Comparative advantage is based on',
+		choices: ['dollar price', 'labor cost', 'capitol cost', 'opportunity cost'],
+		correctAnswer: 3,
+	},
+	{
+		question:
+			'If a company in a perfect competition is making zero profit, what should it do?',
+		choices: [
+			'Increase its price to increase revenue',
+			'Shut down',
+			'Advertise its products',
+			'None of the above',
+		],
+		correctAnswer: 3,
+	},
+	{
+		question: 'In a market economy',
+		choices: [
+			'demand determines supply and supply, in turn, determines prices',
+			'supply determines demand and demand, in turn, determines prices',
+			'supply and demand determine prices and prices, in turn, allocate the economy’s scarce resources',
+			'the allocation of scarce resources determines prices and prices, in turn, determine supply and demand',
+		],
+		correctAnswer: 2,
+	},
+	{
+		question: 'If demand is price inelastic, then',
+		choices: [
+			'buyers do not respond much to a change in price',
+			'buyers respond substantially to a change in price, but the response is very slow',
+			'buyers do not respond much to advertising, fads, or general changes in tastes',
+			'the demand curve is very flat',
+		],
 		correctAnswer: 0,
 	},
 	{
-		question: '',
-		choices: ['', '', '', ''],
+		question: 'What type of economic system is the United States following?',
+		choices: ['command', 'absolute', 'market', 'traditional'],
+		correctAnswer: 2,
+	},
+	{
+		question:
+			'Which of the following is not a type of modern market structure?',
+		choices: [
+			'monopoly',
+			'international market',
+			'oligopoly',
+			'monopolistic competition',
+		],
+		correctAnswer: 1,
+	},
+	{
+		question: 'Market failure is the inability of',
+		choices: [
+			'some unregulated markets to allocate resources efficiently',
+			'a market to establish an equilibrium price',
+			'buyers to place a value on the good or service',
+			'buyers to interact harmoniously with sellers in the market',
+		],
 		correctAnswer: 0,
 	},
 	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
+		question:
+			'Within a country, the domestic price of a product will equal the world price if',
+		choices: [
+			'trade restrictions are imposed on the product',
+			'the country chooses to import, but not export, the product',
+			'the country chooses to export, but not import, the product',
+			'the country allows free trade',
+		],
+		correctAnswer: 3,
 	},
 	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
+		question:
+			'Which one of the following is not a reason why companies give student discount?',
+		choices: [
+			'Discount is a form of advertising in itself',
+			'Obtaining the royalty of younger customers',
+			'Students may not buy the product otherwise, captures wider range of customer',
+			'They care about students',
+		],
+		correctAnswer: 2,
 	},
 	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
-	},
-	{
-		question: '',
-		choices: ['', '', '', ''],
-		correctAnswer: 0,
+		question:
+			'Which of the following best describes the concept of average total cost?',
+		choices: [
+			'The cost that does not change per unit of output',
+			'The cost of producting one unit of output',
+			'The cost of producting one more unit of output',
+			'The cost that varies as output changes for every unit of output',
+		],
+		correctAnswer: 1,
 	},
 ];
 
