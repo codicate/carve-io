bb9048673d6018e5f57618640cd4fe6d73c11f62
codicate <codicate@gmail.com>
2022-06-12 23:35:38 -0400

    instructions

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # carve.io
 
 territory carving local multiplayer board game inspired by splix.io
+
+### How to play
+
+- the goal is to claim the most tiles
+- enclose an area to claim all the tiles inside it
+- if you touch another player's head, the player will lose all progress and restart
+- if you touch another player's tail, the player will lose their tail but not their claimed area
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import s from './App.module.scss';
 import Game from './components/Game';
 import Button from './components/Button';
-import { GoMarkGithub } from 'react-icons/go';
+import { AiFillQuestionCircle, AiOutlineGithub } from 'react-icons/ai';
 
 function App() {
 	return (
 		<div id={s.main}>
 			<Game />
-			<Button
-				styledAs="icon"
-				id={s.github}
-				onClick={() => {
-					window.open('https://github.com/codicate/carve-io/', '_blank');
-				}}
-			>
-				<GoMarkGithub />
-			</Button>
+			<div id={s.info}>
+				<Button
+					styledAs="icon"
+					onClick={() => {
+						window.open('https://github.com/codicate/carve-io/', '_blank');
+					}}
+				>
+					<AiFillQuestionCircle />
+				</Button>
+				<Button
+					styledAs="icon"
+					onClick={() => {
+						window.open('https://github.com/codicate/carve-io/', '_blank');
+					}}
+				>
+					<AiOutlineGithub />
+				</Button>
+			</div>
 		</div>
 	);
 }
