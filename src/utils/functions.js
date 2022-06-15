d8377ef559024ddd7bb2acd41fb150e2995816aa
codicate <codicate@gmail.com>
2022-06-15 13:07:01 -0400

    deleted used files

--- src/utils/functions.js
+++
@@ -1,5 +0,0 @@
-export const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
-
-export const getRandNumBetween = (min, max) => {
-	return Math.floor(Math.random() * (max - min) + min);
-};
